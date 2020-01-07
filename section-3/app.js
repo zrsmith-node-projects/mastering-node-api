b5c8f1c326352fbf6e108a04f0e33bc9e032d1fc
Zac Smith <zrs3141592@gmail.com>
2020-01-07 00:54:05 -0700

    multiple ways to create some basic routes

--- section-3/app.js
+++ section-3/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
 
+// app.get("/api/employees", (req, res) => {
+//   res.send("Hello from Express");
+// });
+// app.post("/api/employees", (req, res) => {
+//   res.send("Hello from POST from Express");
+// });
+
+// app.all("/api/employees", (req, res) => {
+//   res.send(`${req.method} in action`);
+// });
+
+// app
+//   .route("/api/employees")
+//   .get((req, res) => {
+//     res.send("GET");
+//   })
+//   .post((req, res) => {
+//     res.send("POST");
+//   });
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
